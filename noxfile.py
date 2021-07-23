f27f06121ef15530017add946e13d0a21c100ce1
Guillaume Friloux <guillaume@friloux.me>
2021-07-23 18:51:01 +0200

    Add missing noxfile

---
+++ noxfile.py
@@ -0,0 +1,12 @@
+import nox
+
+
+PYTHON_FILES = [
+    "noxfile.py",
+]
+
+@nox.session(reuse_venv=True)
+def gendoc(session):
+    session.install("sphinx", "sphinx-rtd-theme")
+    session.install("-e", ".")
+    session.run("sphinx-build", "-M", "html", "doc", "build")
